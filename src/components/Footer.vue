f36dbeb1613886e40d89a79ea6dfc54e4d138b13
Steven Gamboa <stevengamboa@MacBook-Pro-de-Profesional.local>
2023-02-03 19:24:26 -0500

    Creamos 2 componentes y los usamos

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <p>
+        Hola soy el parrafo
+    </p>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>hola</div>
+</template>
+
+<script>
+import { onMounted } from "vue"
+export default {
+    setup() {
+        onMounted(() => {
+            console.log("mounted");
+        });
+    },
+    }
+</script>
