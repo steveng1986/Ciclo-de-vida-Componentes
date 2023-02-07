5f67d003a0c5dc1428b01fca66f9b6ac3085d298
Steven Gamboa <stevengamboa@MacBook-Pro-de-Profesional.local>
2023-02-07 18:48:11 -0500

    agregamos el primer watch

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,13 +5,15 @@
 </template>
 
 <script>
-import { ref } from "vue"
+import { ref, watch } from "vue"
 export default {
     setup() {
         const text = ref("new class");
         const counter = ref(0);
-        setInterval(() => counter.value++, 500);
-
+        setInterval(() => counter.value++, 1000)
+        watch(counter, (valor, anterior) => {
+            console.log(valor, anterior);
+        } )
         return {
             text,
             counter,
