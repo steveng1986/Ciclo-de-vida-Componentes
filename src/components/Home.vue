36b1b9511d281ec6d164517d3fa4e6ec5059101b
Steven Gamboa <stevengamboa@MacBook-Pro-de-Profesional.local>
2023-02-07 19:29:29 -0500

    hicimos una funcion computada

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,23 @@
 <template>
-    <div>{{ text }}</div>
-    <div>{{ counter }}</div>
+    <div>{{ fullName }}</div>
 
 </template>
 
 <script>
-import { ref, watch } from "vue"
+import { ref, computed } from "vue"
 export default {
-    setup() {
-        const text = ref("new class");
-        const counter = ref(0);
-        setInterval(() => counter.value++, 1000)
-        watch(counter, (valor, anterior) => {
-            console.log(valor, anterior);
-        } )
-        return {
-            text,
-            counter,
-        }
-    },
-    }
+  setup() {
+    const firstName = ref("Steven");
+    const lastName = ref("Gamboa");
+
+
+    const fullName = computed(() => {
+      return `${firstName.value} ${lastName.value}`;
+    });
+
+    return {
+      fullName,
+    };
+  },
+};
 </script>
